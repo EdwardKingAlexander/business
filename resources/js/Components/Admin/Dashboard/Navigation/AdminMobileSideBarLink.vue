a7385d72845a3d5190d5627596dc372a98c9ff11
Daniel "Edward" Oliver <Edward@RockyMountainWeb.Design>
2023-09-25 16:38:59 -0600

    extracted mobile links and icons into different components

---
+++ resources/js/Components/Admin/Dashboard/Navigation/AdminMobileSideBarLink.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+    routeName: {
+        type: String,
+        required: true
+    },
+    active: {
+        type: Boolean,
+        required: false,
+        default: false
+    }
+    
+});
+
+
+
+
+</script>
+
+
+<template>
+    <!-- Current: "bg-red-800 text-white", Default: "" -->
+    <a 
+    
+    :href="route(routeName)" 
+    :class="`group flex items-center rounded-md px-2 py-2 text-sm font-medium leading-6 
+    ${props.active ? 'bg-red-800 text-white' 
+    : 
+    'text-red-100 hover:bg-red-600 hover:text-white'
+    }`" 
+    aria-current="page" >
+            <slot />
+          </a>
+</template>
+
